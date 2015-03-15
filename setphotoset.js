19ca68344a8ce8ab65d547f4f3296bfe4a852aa0
Rein Bulaong <rein.bulaong@yahoo.com>
2015-03-15 15:56:06 +0800

    Update setphotoset.js

--- setphotoset.js
+++ setphotoset.js
@@ -1,5 +1,5 @@
-function setPhotoset (identifier,userOptions) {
-/*User Options extending default settings*/
+;function setPhotoset(elements,userOptions) {
+	/*User Options extending default settings*/
 	userOptions = userOptions || {};
 	var settings = {
 		layout: 0,	/*sets layout of photoset. If zero, acquires info from data-layout attribute if existing. else the function does nothing*/
@@ -13,63 +13,55 @@ function setPhotoset (identifier,userOptions) {
 	}
 
 
-	/*Iterating per element that matches identifier*/
-	var psets = document.querySelectorAll(identifier);
-	Array.prototype.forEach.call(psets, function (set,i) {
+	/*Iterating per element that matches element*/
+	Array.prototype.forEach.call(elements, function (set,i) {
 		var lay = settings.layout || set.getAttribute("data-layout") || "";
 		var els = set.children;
 		var cLay, cOff = 0, cEl = 0;
 		var x,y;
 		var a = [], b = []		
 		
-		/*We define the actual functionality inside a function so that we can attach it to an event handler if pData is false*/
-		function setInnerFunction () {
-			/*Children are handled by rows*/
-			for (x = 0; x<lay.length; x++) {
-				cLay = parseInt(lay.charAt(x));
-				
-				/* Floats children to the left. First of every row gets clear: left */
-				for (y = 0 ; y < cLay; y++) {
-					els[cEl].style.float = "left";
-					if (y==0) els[cEl].style.clear = "left";
-					cEl++;
-				}
-				
-				/*stores aspect ratios of elements in an array a*/
-				for ( y = 0 ; y < cLay ; y++) {
-					if (settings.pData) {
-						a[y] = parseInt(els[cOff+y].getAttribute(settings.childHeight)) / parseInt(els[cOff+y].getAttribute(settings.childWidth));
-						}
-					else {
-						a[y] = els[cOff+y].offsetHeight/els[cOff+y].offsetWidth;
-					}
-				}
-				
-				/*calculates percentage width of children*/
-				for (y = 0; y < cLay; y++) {
-					if (y==0) {
-						for(k = 0, z = 0; z < cLay; z++) { k += (a[0]/a[z])}
-						b[y] = 100/k;
-					}
-					else if (y==cLay-1) {
-						for(k = 0, z = 0; z < cLay-1; z++) { k += b[z]}
-						b[y] = 100 - k;
-					}
-					else {
-						b[y] = b[0]*(a[0]/a[y]);
-					}
-					els[cOff+y].style.width = b[y] + "%";
-				}
-				cOff += cLay;
+	
+		/*Children are handled by rows*/
+		for (x = 0; x<lay.length; x++) {
+			cLay = parseInt(lay.charAt(x));
+			
+			/* Floats children to the left. First of every row gets clear: left */
+			for (y = 0 ; y < cLay; y++) {
+				els[cEl].style.float = "left";
+				if (y==0) els[cEl].style.clear = "left";
+				cEl++;
 			}
+			
+			/*stores aspect ratios of children in an array a*/
+			for ( y = 0 ; y < cLay ; y++) {
+				if (settings.pData) {
+					a[y] = parseInt(els[cOff+y].getAttribute(settings.childHeight)) / parseInt(els[cOff+y].getAttribute(settings.childWidth));
+					}
+				else {
+					if (els[cOff+y].naturalWidth) a[y] = els[cOff+y].naturalHeight/els[cOff+y].naturalWidth;
+					else a[y] = els[cOff+y].offsetHeight/els[cOff+y].offsetWidth;
+				}
+			}
+			
+			/*calculates percentage width of children*/
+			for (y = 0; y < cLay; y++) {
+				if (y==0) {
+					for(k = 0, z = 0; z < cLay; z++) { k += (a[0]/a[z])}
+					b[y] = 100/k;
+				}
+				else if (y==cLay-1) {
+					for(k = 0, z = 0; z < cLay-1; z++) { k += b[z]}
+					b[y] = 100 - k;
+				}
+				else {
+					b[y] = b[0]*(a[0]/a[y]);
+				}
+				els[cOff+y].style.width = b[y] + "%";
+			}
+			cOff += cLay;
 		}
-
-		/*setInnerFunction Triggers automatically if pData is true*/
-		if (pData) setInnerFunction();
-		else set.addEventListener("load",setInnerFunction);
 		
 		
 	});
-	
-	return lay;
 }
